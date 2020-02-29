1dc20edd20237729d2ffdbccf96c978151ff600e
Richard Bobo <richard.bobo@gmail.com>
2020-02-29 17:54:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,11 +16,12 @@ end
 
 def using_first(array)
    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.reverse 
+  array.first
 end
 
 def using_last(array)
-
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.last 
 end
 
 def using_size(array)
